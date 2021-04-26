9f850d648c17ec1c41b6eff96f49b66348165044
steklonoid <40269765+steklonoid@users.noreply.github.com>
2021-04-26 18:01:45 +0300

    Разбираюсь с многопоточностью. 26/04/2021

--- wss.py
+++ wss.py
@@ -6,7 +6,7 @@ import time
 class WSThread(Thread):
 
     methods = {'subscribe':1, 'unsubscribe':2, 'subscriptions':3, 'auth':4, 'placeOrder':5, 'cancelOrder':6, 'cancelAllOrders':7, 'placeCondOrder':8, 'cancelCondOrder':9, 'closeContract':10, 'closePosition':11, 'getTraderStatus':12, 'changeLeverageAll':13}
-    channels = ['orderbook_1', 'kline', 'trades', 'liquidations', 'ticker', 'fundingInfo', 'index', 'tradingStatus', 'orderStatus', 'orderFilled', 'orderCancelled', 'condOrderStatus', 'contractClosed', 'traderStatus', 'leverage', 'funding', 'position']
+
 
     def __init__(self, pc):
         super(WSThread, self).__init__()
@@ -27,18 +27,18 @@ class WSThread(Thread):
             if message == 'ping':
                 wssapp.send('pong')
             else:
-                try:
-                    self.message = json.loads(message)
-                except:
-                    return
+                self.message = json.loads(message)
                 if self.message.get('id'):
                     id = self.message.get('id')
                     status = self.message.get('status')
                     self.pc.message_response(id, status)
                 if self.message.get('ch'):
-                    self.pc.message_queue.append(self.message)
-                    # channel = self.message.get('ch')
-                    # data = self.message.get('data')
+                    channel = self.message.get('ch')
+                    data = self.message.get('data')
+                    self.pc.channels[channel].append(data)
+                    # print(self.pc.channels[channel])
+                    #
+                    #
                     # if channel in self.channels:
                     #     eval('self.pc.message_' + channel + '(data)')
 
@@ -61,22 +61,26 @@ class WSThread(Thread):
     def send_privat(self, method, **params):
         pd = {'id':self.methods.get(method), 'method':method, 'params':params}
         strpar = json.dumps(pd)
-        try:
-            self.wsapp.send(strpar)
-        except:
-            print('Ошибка')
+        self.wsapp.send(strpar)
         time.sleep(0.1)
 
 class SuperViser(Thread):
     def __init__(self, pc):
         super(SuperViser, self).__init__()
         self.pc = pc
-        self.timer = 0.2
+        self.timer = 0.5
 
     def run(self) -> None:
+        oldtime = time.time()
         while True:
-            if self.pc.message_queue:
-                print(len(self.pc.message_queue))
-                mes = self.pc.message_queue[0]
-                del self.pc.message_queue[0]
-                time.sleep(self.timer)
+            time.sleep(self.timer)
+            newtime = time.time()
+            print(newtime - oldtime)
+            oldtime = newtime
+
+            for channel in self.pc.channels.keys():
+                listmes = self.pc.channels[channel]
+                if listmes:
+                    data = listmes[-1]
+                    eval('self.pc.message_' + channel + '(data)')
+                    listmes.clear()
