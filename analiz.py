7cac11a6567bc853fe25f42f4e2f2b82562b960b
steklonoid <40269765+steklonoid@users.noreply.github.com>
2021-05-18 17:09:08 +0300

    18/05/2021 Начало разработки нейросети

--- analiz.py
+++ analiz.py
@@ -1,17 +1,45 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-TRAIN = 60000
-VAL = 30000
-TEST = 18000
+from keras import models
+from keras import layers
 
-NUM = 512
+NUM = 1000
+LOOKBACK = 16
 
 arr = np.loadtxt('history.csv', delimiter=',')
 arrlen = arr.shape[0]
 
-# plt.plot(arr[:, 0], arr[:, 1])
-# plt.show()
+arrsin = np.zeros((NUM, 2))
+for i in range(NUM):
+    arrsin[i] = [i, (np.sin(20 * i / NUM) + 1) / 2]
 
-arr1 = np.zeros((10, 5, 2))
-print(arr1)
+x = np.zeros((NUM - LOOKBACK - 1, LOOKBACK))
+y = np.zeros((NUM - LOOKBACK - 1, ))
+
+for i in range(NUM - LOOKBACK - 1):
+    x[i] = arrsin[i:i + LOOKBACK, 1]
+    y[i] = arrsin[i + LOOKBACK + 1, 1]
+
+x_test = x[:700]
+x_val = x[700:]
+y_test = y[:700]
+y_val = y[700:]
+
+model = models.Sequential()
+model.add(layers.Dense(LOOKBACK // 2,  activation='relu', input_shape=(LOOKBACK,)))
+model.add(layers.Dense(LOOKBACK // 4,  activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+ loss='binary_crossentropy',
+ metrics=['accuracy'])
+
+history = model.fit(x_test, y_test, epochs=20, batch_size=32, validation_data=(x_val, y_val))
+
+predictions = model.predict(x)
+print(predictions)
+
+# plt.plot(arrsin[:, 0], arrsin[:, 1])
+plt.plot(predictions)
+plt.show()
